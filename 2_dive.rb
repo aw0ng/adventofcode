56bec48ad5f9f5cc5e72fba40dc4d2768c6c9819
Anna Wong <76626238+aw0ng@users.noreply.github.com>
2021-12-14 21:49:04 -0500

    parse dive data

--- 2_dive.rb
+++ 2_dive.rb
@@ -27,3 +27,6 @@
 # After following these instructions, you would have a horizontal position of 15 and a depth of 10. (Multiplying these together produces 150.)
 
 # Calculate the horizontal position and depth you would have after following the planned course. What do you get if you multiply your final horizontal position by your final depth?
+
+sample_input = File.readlines("2_dive.csv").map{ |x| x.split }
+p sample_input
