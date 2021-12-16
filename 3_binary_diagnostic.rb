7d75aec01d28a398a60e7ffe6df384899384e7ac
Anna Wong <76626238+aw0ng@users.noreply.github.com>
2021-12-15 23:07:17 -0500

    pseudo code for binary with sample input

--- 3_binary_diagnostic.rb
+++ 3_binary_diagnostic.rb
@@ -18,6 +18,7 @@
 # 11001
 # 00010
 # 01010
+
 # Considering only the first bit of each number, there are five 0 bits and seven 1 bits. Since the most common bit is 1, the first bit of the gamma rate is 1.
 
 # The most common second bit of the numbers in the diagnostic report is 0, so the second bit of the gamma rate is 0.
@@ -27,5 +28,31 @@
 # So, the gamma rate is the binary number 10110, or 22 in decimal.
 
 # The epsilon rate is calculated in a similar way; rather than use the most common bit, the least common bit from each position is used. So, the epsilon rate is 01001, or 9 in decimal. Multiplying the gamma rate (22) by the epsilon rate (9) produces the power consumption, 198.
+
+sample_input = [ 00100, 11110, 10110, 10111, 10101, 01111, 00111, 11100, 10000, 11001, 00010, 01010 ]
+
+def gamma(input)
+  zeros = 0
+  ones = 0
+  first_gamma = 0
+  i = 0
+  while i < input.length
+    if input[i][0] == 0
+      zeros += 1
+    else input[i][0] == 1
+         ones += 1
+    end
+    i += 1
+  end
+  p zeros
+  # if zeros > ones
+  #   first_gamma == 0
+  # else
+  #   first_gamma == 1
+  # end
+  # p first_gamma
+end
+
+gamma(sample_input)
 
 # Use the binary numbers in your diagnostic report to calculate the gamma rate and epsilon rate, then multiply them together. What is the power consumption of the submarine? (Be sure to represent your answer in decimal, not binary.)
